9618edf2691c20bca77c6ef7ca396625e9185806
Roman Kyrkalo <roman.kyrkalo@live.com>
2024-10-02 18:26:36 -0400

    add simple c# code - token service remove unused files

--- src/server/Lite/Lite.Api/Program.cs
+++ src/server/Lite/Lite.Api/Program.cs
@@ -1,3 +1,13 @@
+using AspNetCore.Identity.MongoDbCore.Models;
+using Lite.Api.Models;
+using Lite.Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,39 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var configuration = builder.Configuration;
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+IConfigurationSection jwtSettings = configuration.GetSection("JwtSettings");
+IConfigurationSection mongoSettings = configuration.GetSection("MongoDbSettings");
+var key = Encoding.ASCII.GetBytes(jwtSettings["Secret"]);
+
+builder.Services.AddIdentity<ApplicationUser, MongoIdentityRole>()
+.AddMongoDbStores<ApplicationUser, MongoIdentityRole, Guid>(mongoSettings["ConnectionString"], mongoSettings["DatabaseName"])
+.AddDefaultTokenProviders();
+
+// JWT configuration
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = true,
+        ValidateLifetime = true
+    };
+});
 
 var app = builder.Build();
 
@@ -23,3 +66,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class MongoDbContextOptions
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
---
+++ src/server/Lite/Lite.Api/Services/TokenService.cs
@@ -0,0 +1,65 @@
+﻿using Lite.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Lite.Api.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly ILogger _logger;
+    private readonly IConfigurationSection _jwtSettings;
+
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _jwtSettings = _configuration.GetSection("JwtSettings");
+    }
+
+    public ClaimsPrincipal Validate(string token)
+    {
+        var key = Encoding.ASCII.GetBytes(_jwtSettings["Secret"]);
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true
+            }, out SecurityToken validatedToken);
+
+            return principal;
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+        return default;
+    }
+
+    public string Generate(ApplicationUser user)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings["Secret"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+        var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddMinutes(60),
+        signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
